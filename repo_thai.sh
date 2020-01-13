6347a8157440adf15a67a2d054bd9b96eaa32a01
akkradet <akkradet7@gmail.com>
2020-01-13 13:22:23 +0700

    Update repo_thai.sh

--- repo_thai.sh
+++ repo_thai.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 cd /etc/
-mv /etc/yum.repos.d /etc/yum.repos.d.backup4comex
+mv /etc/yum.repos.d /etc/yum.repos.d.backup
 mkdir /etc/yum.repos.d
 wget -O /etc/yum.repos.d/CentOS-Base.repo http://mirror1.ku.ac.th/yum-centos/7/CentOS-Base.repo
 yum clean all
