af4d9905cf80d68f17696d29135a8b10ed4e6eea
akkradet <akkradet7@gmail.com>
2020-01-13 13:21:59 +0700

    Add files via upload

---
+++ repo_thai.sh
@@ -0,0 +1,7 @@
+#!/bin/sh
+cd /etc/
+mv /etc/yum.repos.d /etc/yum.repos.d.backup4comex
+mkdir /etc/yum.repos.d
+wget -O /etc/yum.repos.d/CentOS-Base.repo http://mirror1.ku.ac.th/yum-centos/7/CentOS-Base.repo
+yum clean all
+yum makecache
